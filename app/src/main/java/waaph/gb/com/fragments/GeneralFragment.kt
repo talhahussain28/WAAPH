8d95f1a1944d1d81ee3e139e79cb1f7cbb35fb54
walee <waleed@miletap.com>
2022-03-31 19:32:57 +0500

    General Fragment new UI

--- app/src/main/java/waaph/gb/com/fragments/GeneralFragment.kt
+++ app/src/main/java/waaph/gb/com/fragments/GeneralFragment.kt
@@ -1,44 +1,49 @@
 package waaph.gb.com.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.fragment_general.*
+import waaph.gb.com.CreateGeneralDataActivity
+import waaph.gb.com.CustomerDataFormActivity
 import waaph.gb.com.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GeneralFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class GeneralFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class GeneralFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_general, container, false)
-    }
 
-    companion object fun newInstance(){
+        return   inflater.inflate(R.layout.fragment_general, container, false)
 
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        clickListners()
+
+    }
+
+    override fun onClick(v: View?) {
+
+        when (v?.id) {
+            R.id.routeToCreateGeneral -> {
+                val intent = Intent(requireActivity(), CreateGeneralDataActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+    }
+
+    private fun clickListners(){
+        routeToCreateGeneral.setOnClickListener(this)
+
+    }
+
 }
