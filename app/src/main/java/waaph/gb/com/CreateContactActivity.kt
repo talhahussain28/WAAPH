b6dac3b09e26fa8f4cfa3f072cc6549ada7b8ade
walee <waleed@miletap.com>
2022-04-16 01:09:33 +0500

    All Bottom Sheet Done

--- app/src/main/java/waaph/gb/com/CreateContactActivity.kt
+++ app/src/main/java/waaph/gb/com/CreateContactActivity.kt
@@ -1,12 +1,22 @@
 package waaph.gb.com
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.view.WindowManager
+import android.widget.AdapterView
+import android.widget.ListView
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.activity_create_bank.*
 import kotlinx.android.synthetic.main.activity_create_contact.*
 import kotlinx.android.synthetic.main.activity_create_general_data.*
+import kotlinx.android.synthetic.main.custom_dialog.*
+import waaph.gb.com.utils.GeneralBottomAdapter
 
 class CreateContactActivity : AppCompatActivity(), View.OnClickListener  {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,17 +31,65 @@ class CreateContactActivity : AppCompatActivity(), View.OnClickListener  {
             R.id.btnNext -> {
                 addAddress()
             }
+            R.id.openCategoryList -> {
+                pickerActionDialog()
+            }
         }
     }
 
-    private fun etValidate(edittext: TextInputEditText): Boolean {
-        var validate = edittext.text.toString()
-        validate = validate.replace("\\s+".toRegex(), " ").trim { it <= ' ' }
-        if (validate.isEmpty()) {
-            edittext.error = "Required"
-            return false
-        }
-        return true
+    private fun pickerActionDialog() {
+
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.custom_dialog)
+        dialog.window!!.attributes.windowAnimations = R.style.DialogAnimation_2
+        dialog.window!!.setGravity(Gravity.BOTTOM)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window!!.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+
+        dialog.setCancelable(true)
+        dialog.setCanceledOnTouchOutside(true)
+
+        val listView = dialog.findViewById<ListView>(R.id.listView)
+        val list: MutableList<GeneralBottomAdapter.ListItemModel> = ArrayList()
+
+        list.add(GeneralBottomAdapter.ListItemModel("waleed"))
+        list.add(GeneralBottomAdapter.ListItemModel("anus ali"))
+        list.add(GeneralBottomAdapter.ListItemModel("Option 3"))
+        list.add(GeneralBottomAdapter.ListItemModel("Option 4"))
+        list.add(GeneralBottomAdapter.ListItemModel("Option 5"))
+
+        val adapter = GeneralBottomAdapter(this, "", list)
+        listView.adapter = adapter
+        dialog.listView.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                val data = parent.getItemAtPosition(position)
+                        as GeneralBottomAdapter.ListItemModel
+
+                when (position) {
+                    0 -> {
+                        openCategoryList.text = "waleed"
+                    }
+                    1 -> {
+                        openCategoryList.text = "anus ali"
+                    }
+                    2 -> {
+                        openCategoryList.text = "Option 3"
+                    }
+                    3 -> {
+                        openCategoryList.text = "Option 4"
+                    }
+                    4 -> {
+                        openCategoryList.text = "Option 5"
+                    }
+                }
+                dialog.dismiss()
+
+            }
+        dialog.show()
+
     }
 
     private fun addAddress(){
@@ -49,7 +107,18 @@ class CreateContactActivity : AppCompatActivity(), View.OnClickListener  {
         }
     }
 
+    private fun etValidate(edittext: TextInputEditText): Boolean {
+        var validate = edittext.text.toString()
+        validate = validate.replace("\\s+".toRegex(), " ").trim { it <= ' ' }
+        if (validate.isEmpty()) {
+            edittext.error = "Required"
+            return false
+        }
+        return true
+    }
+
     private fun setOnClickListener(){
         btnNext.setOnClickListener(this)
+        openCategoryList.setOnClickListener(this)
     }
 }
