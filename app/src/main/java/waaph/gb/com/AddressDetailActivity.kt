a13eb471c5dfb6f7bc7c8db9a1b5657f28ed34f3
walee <waleed@miletap.com>
2022-04-20 16:14:42 +0500

    UI DONE

---
+++ app/src/main/java/waaph/gb/com/AddressDetailActivity.kt
@@ -0,0 +1,12 @@
+package waaph.gb.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddressDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_address_detail)
+
+    }
+}
---
+++ app/src/main/java/waaph/gb/com/ContactDetailActivity.kt
@@ -0,0 +1,11 @@
+package waaph.gb.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_detail)
+    }
+}
