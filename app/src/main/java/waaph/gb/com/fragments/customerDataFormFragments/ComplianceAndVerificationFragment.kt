5728849a809dbba109716e070e769cf7ca909814
Mynameishussain8 <muhammadtalhahussain28@gmail.com>
2022-06-16 04:03:51 +0500

    camera in process

--- app/src/main/java/waaph/gb/com/fragments/customerDataFormFragments/ComplianceAndVerificationFragment.kt
+++ app/src/main/java/waaph/gb/com/fragments/customerDataFormFragments/ComplianceAndVerificationFragment.kt
@@ -1,16 +1,28 @@
 package waaph.gb.com.fragments.customerDataFormFragments
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.fragment_compliance_and_verification.*
 import waaph.gb.com.CustomerDataFormActivity
 import waaph.gb.com.R
 import waaph.gb.com.utils.BaseFragment
 
 class ComplianceAndVerificationFragment : BaseFragment(),View.OnClickListener {
+
+    private var REQUEST_CODE = -1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +44,7 @@ class ComplianceAndVerificationFragment : BaseFragment(),View.OnClickListener {
 
     override fun setOnClickListener() {
         next.setOnClickListener(this)
+        img.setOnClickListener(this)
     }
 
     override fun initialize() {
@@ -47,7 +60,72 @@ class ComplianceAndVerificationFragment : BaseFragment(),View.OnClickListener {
                 (activity as CustomerDataFormActivity).setCurrentItem(6)
 
             }
+            R.id.img -> {
+                capturePhoto()
+            }
 
         }
     }
+
+    fun capturePhoto() {
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, REQUEST_CODE)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE && data != null){
+            text.visibility = View.GONE
+            cnic.setImageBitmap(data.extras?.get("data") as Bitmap)
+        }
+    }
+    /*fun isPermissionsAllowed(): Boolean {
+        return if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            false
+        } else true
+    }
+
+    fun askForPermissions(): Boolean {
+        if (!isPermissionsAllowed()) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this as Activity,Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                showPermissionDeniedDialog()
+            } else {
+                ActivityCompat.requestPermissions(this as Activity,arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),REQUEST_CODE)
+            }
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,permissions: Array<String>,grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission is granted, you can perform your operation here
+                } else {
+                    // permission is denied, you can ask for permission again, if you want
+                    //  askForPermissions()
+                }
+                return
+            }
+        }
+    }
+
+    private fun showPermissionDeniedDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Permission Denied")
+            .setMessage("Permission is denied, Please allow permissions from App Settings.")
+            .setPositiveButton("App Settings",
+                DialogInterface.OnClickListener { dialogInterface, i ->
+                    // send to app settings if permission is denied permanently
+                    val intent = Intent()
+                    intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                    val uri = Uri.fromParts("package", getPackageName(), null)
+                    intent.data = uri
+                    startActivity(intent)
+                })
+            .setNegativeButton("Cancel",null)
+            .show()
+    }*/
 }
